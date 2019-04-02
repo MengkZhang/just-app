d9132ca55d18ffa9461a79fa09d7574883534b1b
zhangxiaolin <zhangxiaolin@tzpt.com>
2019-04-02 14:36:00 +0800

    V1.0.00:配置了项目的签名和混淆文件-为代码开发作好了准备

---
+++ app/src/main/java/com/mengk/justapp/JustApplication.java
@@ -0,0 +1,14 @@
+package com.mengk.justapp;
+
+import android.app.Application;
+
+/**
+ * Created by Mengk on 2019/4/2
+ * Describe :
+ */
+public class JustApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
